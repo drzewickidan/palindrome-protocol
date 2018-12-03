5dcaea28b2ad159ac09f62aa8acdcc55fe0283cc
drzewickidan <drzewickidan@gmail.com>
2018-12-03 14:44:42 -0500

    Expanded commments

--- client.py
+++ client.py
@@ -1,52 +1,54 @@
 import socket
 import sys
 
-#address and port of host
+# address and port of server
+# for ECEC531, this is hardcoded to run on localhost:5000
 host = '127.0.0.1'
 port = 5000
 
 
 def client():
-    #create connection client
+    # create client obeject
     s = socket.socket()
     s.connect((host, port))
-    
-    #check amount of arguments
+
     if len(sys.argv) == 2:
         # non-persistent connection
         if sys.argv[1] == "-np" or sys.argv[1] == "--non-persistent":
-            #save user input as msg
+            # user input to send to server
             msg = raw_input()
-            #check if QUIT command was used
+            # check if QUIT command was used
             if not msg == "QUIT":
-                #send msg
+                # send command
                 s.send(msg)
-                #receive response from server
+                # receive response from server
                 data = s.recv(1024)
-                #display data received from server
+                # display data received from server
                 print str(data)
-            #if QUIT was entered, send to server and close connection
+            # if QUIT was entered, first tell the server to close connection and then close socket
             s.send("QUIT")
             s.close()
         else:
             raise Exception("unrecognized argument")
     elif len(sys.argv) > 2:
+        # if the amount of arguments are greater than two (client.py <arg1> [<arg2>] raise an Exception)
         raise Exception("too many arguments")
+    # default: persistent connection
     else:
-        # default: persistent connection
-        #use user input as msg
+        # user input to send to server
         msg = raw_input()
         while True:
-            #if QUIT is entered, send to server and break loop to close connection
+            # if QUIT is entered, send to server and break loop to close connection
             if msg == "QUIT":
                 s.send(msg)
                 break
-            #send msg
+            # send command
             s.send(msg)
-            #receive response from server
+            # receive response from server
             data = s.recv(1024)
-            #display data received from server
+            # display data received from server
             print str(data)
+            # continue sending commands
             msg = raw_input()
         s.close()
 
--- server.py
+++ server.py
@@ -1,74 +1,103 @@
 import socket
 import threading
 
-#create server with specified parameters
+
 class ThreadedServer(object):
+    """A TCP server that listens to multiple clients
+
+    When a new client attempts to connect, this class will create a new thread to listen and respond
+    to that particular client. Each client can send the following commands:
+        VRFY <word> [<word> ...] : verifies whether a word is a palindrome or multiple words are palindromes
+        LIST : return a list of palindromes found including the palindromes other clients have found
+        TOP : return the last palindrome found
+        RETR <#> : return a particular palindrome from the LIST command
+        STAT : return the number of palindromes found
+        DELE <#> : remove a particular palindrome from the LIST command
+        REST : reset the list of palindromes
+        QUIT : end the session with the server
+
+    """
+
     def __init__(self, host, port):
+        """Initializes ThreadedServer
+
+        Binds a TCP socket to an IP and port and initializes a list of palindromes that each thread shares
+
+        :param host: Host IP address in the form a.b.c.d
+        :param port: Host port to accept connections from clients
+
+        """
         self.host = host
         self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STRDR, 1)EAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEAD
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.host, self.port))
         self.palindromes = []
 
     def listen(self):
+        """Listen for new clients trying to connect and spawn a thread to listen to their commands"""
         self.sock.listen(5)
         while True:
             client, address = self.sock.accept()
-            client.settimeout(60)
             threading.Thread(target=self.listen_to_client, args=(client, address)).start()
 
     def listen_to_client(self, client, address):
-        #indicate that connection was successful
+        """Listen for commands from a particular client
+        :param client: socket object usable to send and receive data on the connection
+        :param address: address bound to the socket on the other end of the connection
+
+        """
+        # print welcome message to indicate which client this thread is listening to
         print "connection established to %s,%s" % (address[0], address[1])
         while True:
             try:
-                #attempt to receive data
+                # attempt to receive data
                 data = client.recv(1024)
                 if data:
                     cmd = data.split(' ')
-                    #check if list was requested
+                    # check if list was requested
                     if str(cmd[0]) == "LIST":
                         if self.palindromes:
                             response = ""
-                            #add each item in palindrome list to response
+                            # add each item in palindrome list to response
                             for num, line in enumerate(self.palindromes, 1):
                                 response += "%s %s\n" % (num, line)
-                            response = response[:-1]  # remove extra newline
+                            # remove extra newline
+                            response = response[:-1]
                         else:
-                            #indicate if list is empty
                             response = "palindrome list is empty"
-                    #returns palindrome at specified index upon receiving RETR command
+                    # returns palindrome at specified index upon receiving RETR command
                     elif str(cmd[0]) == "RETR":
                         if int(cmd[1]) - 1 < len(self.palindromes) and not int(cmd[1]) <= 0:
                             response = str(self.palindromes[int(cmd[1])-1])
                         else:
                             response = "palindrome %s does not exist" % str(cmd[1])
-                    #returns last palindrome found when TOP command received
+                    # returns last palindrome found when TOP command received
                     elif str(cmd[0]) == "TOP":
                         if self.palindromes:
                             response = "%s" % self.palindromes[-1]
                         else:
                             response = "palindrome list is empty"
-                    #clear palindrome at specified position when DELE command received
+                    # clear palindrome at specified position when DELE command received
                     elif str(cmd[0]) == "DELE":
                         if int(cmd[1]) - 1 < len(self.palindromes) and not int(cmd[1]) <= 0:
                             del self.palindromes[int(cmd[1])-1]
                             response = "deleted palindrome %s" % str(cmd[1])
                         else:
                             response = "palindrome %s does not exist" % str(cmd[1])
-                    #clear list of palindromes when RSET command received
+                    # clear list of palindromes when RSET command received
                     elif str(cmd[0]) == "RSET":
                         self.palindromes = []
                         response = "reset list of palindromes"
-                    #indicate number of palindromes found when STAT command received
+                    # indicate number of palindromes found when STAT command received
                     elif str(cmd[0]) == "STAT":
                         if len(self.palindromes) == 1:
                             response = "1 palindrome found"
                         else:
                             response = "%s palindromes found" % len(self.palindromes)
-                    #check for palindrome when VRFY command received
+                    # check for palindrome when VRFY command received
                     elif str(cmd[0]) == "VRFY":
+                        # dynamically verify each word that the client sends
                         response = ""
                         for i in range(1, len(cmd)):
                             if self.is_palindrome(str(cmd[i])):
@@ -76,14 +105,15 @@ class ThreadedServer(object):
                                 response += str(cmd[i]) + " is a palindrome\n"
                             else:
                                 response += str(cmd[i]) + " is not a palindrome\n"
-                        response = response[:-1]  # remove extra newline
-                    #close connection upon QUIT command
+                        # remove extra newline
+                        response = response[:-1]
+                    # close connection upon QUIT command
                     elif str(cmd[0]) == "QUIT":
                         self.close_client(client, address)
                         return False
                     else:
                         response = "unrecognized command"
-                    #send response back to client
+                    # send response back to client
                     client.send(response)
             except Exception as e:
                 self.close_client(client, address)
@@ -92,14 +122,17 @@ class ThreadedServer(object):
 
     @staticmethod
     def close_client(client, address):
-        #close client and print status message
+        """Close a socket and print exit message"""
         print "closing connection to %s,%s" % (address[0], address[1])
         client.close()
 
     @staticmethod
     def is_palindrome(s):
+        """Return whether a string is a palindrome or not"""
         return s == s[::-1]
 
 
 if __name__ == "__main__":
+    # executed when server module is run directly
+    # for ECEC531, this is hardcoded to run on localhost:5000
     ThreadedServer('127.0.0.1', 5000).listen()
