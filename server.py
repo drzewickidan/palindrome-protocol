80494df3fceaa7d562415e3154efbf556f4059b9
brianktu <45187223+brianktu@users.noreply.github.com>
2018-12-02 23:46:55 -0500

    Added Comments

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import socket
 import threading
 
-
+#create server with specified parameters
 class ThreadedServer(object):
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STRDR, 1)EAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEAD
         self.sock.bind((self.host, self.port))
         self.palindromes = []
 
@@ -19,44 +19,55 @@ class ThreadedServer(object):
             threading.Thread(target=self.listen_to_client, args=(client, address)).start()
 
     def listen_to_client(self, client, address):
+        #indicate that connection was successful
         print "connection established to %s,%s" % (address[0], address[1])
         while True:
             try:
+                #attempt to receive data
                 data = client.recv(1024)
                 if data:
                     cmd = data.split(' ')
+                    #check if list was requested
                     if str(cmd[0]) == "LIST":
                         if self.palindromes:
                             response = ""
+                            #add each item in palindrome list to response
                             for num, line in enumerate(self.palindromes, 1):
                                 response += "%s %s\n" % (num, line)
                             response = response[:-1]  # remove extra newline
                         else:
+                            #indicate if list is empty
                             response = "palindrome list is empty"
+                    #returns palindrome at specified index upon receiving RETR command
                     elif str(cmd[0]) == "RETR":
                         if int(cmd[1]) - 1 < len(self.palindromes) and not int(cmd[1]) <= 0:
                             response = str(self.palindromes[int(cmd[1])-1])
                         else:
                             response = "palindrome %s does not exist" % str(cmd[1])
+                    #returns last palindrome found when TOP command received
                     elif str(cmd[0]) == "TOP":
                         if self.palindromes:
                             response = "%s" % self.palindromes[-1]
                         else:
                             response = "palindrome list is empty"
+                    #clear palindrome at specified position when DELE command received
                     elif str(cmd[0]) == "DELE":
                         if int(cmd[1]) - 1 < len(self.palindromes) and not int(cmd[1]) <= 0:
                             del self.palindromes[int(cmd[1])-1]
                             response = "deleted palindrome %s" % str(cmd[1])
                         else:
                             response = "palindrome %s does not exist" % str(cmd[1])
+                    #clear list of palindromes when RSET command received
                     elif str(cmd[0]) == "RSET":
                         self.palindromes = []
                         response = "reset list of palindromes"
+                    #indicate number of palindromes found when STAT command received
                     elif str(cmd[0]) == "STAT":
                         if len(self.palindromes) == 1:
                             response = "1 palindrome found"
                         else:
                             response = "%s palindromes found" % len(self.palindromes)
+                    #check for palindrome when VRFY command received
                     elif str(cmd[0]) == "VRFY":
                         response = ""
                         for i in range(1, len(cmd)):
@@ -66,11 +77,13 @@ class ThreadedServer(object):
                             else:
                                 response += str(cmd[i]) + " is not a palindrome\n"
                         response = response[:-1]  # remove extra newline
+                    #close connection upon QUIT command
                     elif str(cmd[0]) == "QUIT":
                         self.close_client(client, address)
                         return False
                     else:
                         response = "unrecognized command"
+                    #send response back to client
                     client.send(response)
             except Exception as e:
                 self.close_client(client, address)
@@ -79,6 +92,7 @@ class ThreadedServer(object):
 
     @staticmethod
     def close_client(client, address):
+        #close client and print status message
         print "closing connection to %s,%s" % (address[0], address[1])
         client.close()
 
