eaa1fab44beda4a55bcb7422566809ecebb1c5a4
brianktu <45187223+brianktu@users.noreply.github.com>
2018-12-02 23:25:10 -0500

    Added comments

--- client.py
+++ client.py
@@ -1,23 +1,31 @@
 import socket
 import sys
 
-
+#address and port of host
 host = '127.0.0.1'
 port = 5000
 
 
 def client():
+    #create connection client
     s = socket.socket()
     s.connect((host, port))
-
+    
+    #check amount of amount of arguments
     if len(sys.argv) == 2:
         # non-persistent connection
         if sys.argv[1] == "-np" or sys.argv[1] == "--non-persistent":
+            #use user input as msg
             msg = raw_input()
+            #check if QUIT was input
             if not msg == "QUIT":
+                #send msg
                 s.send(msg)
+                #receive response from server
                 data = s.recv(1024)
+                #display data received from server
                 print str(data)
+            #if QUIT was entered, send to server and close connection
             s.send("QUIT")
             s.close()
         else:
@@ -26,13 +34,18 @@ def client():
         raise Exception("too many arguments")
     else:
         # default: persistent connection
+        #use user input as msg
         msg = raw_input()
         while True:
+            #if QUIT is entered, send to server and break loop to close connection
             if msg == "QUIT":
                 s.send(msg)
                 break
+            #send msg
             s.send(msg)
+            #receive response from server
             data = s.recv(1024)
+            #display data received from server
             print str(data)
             msg = raw_input()
         s.close()
